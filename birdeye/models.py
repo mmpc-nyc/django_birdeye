4c94822c462cd85c1a29f19406238d5d1951f0bd
Marek Schir <schir2@gmail.com>
2020-10-22 16:49:10 -0400

    Updated Models

--- birdeye/models.py
+++ birdeye/models.py
@@ -2,12 +2,38 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import validate_email
 
-# Create your models here.
-class Checkin(models.Model):
-    timestamp = models.DateTimeField(null=True,blank=True)
-    contactid = models.IntegerField(blank=False,null=False)
-    employeeid = models.IntegerField(blank=False,null=False)
-    employee_email = models.CharField(blank=True,null=True,max_length=128)
-    customer_email = models.CharField(blank=True,null=True,max_length=128)
+
+class Contact(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.CharField(blank=True, null=True, max_length=128, validators=[validate_email], unique=True)
+    phone = models.CharField(blank=True,null=True,max_length=11, unique=True)
+
+    def __str__(self):
+        return f'{self.email if self.email else self.phone}'
+
+    def __unicode__(self):
+        return f'{self.email if self.email else self.phone}'
+
+
+class Employee(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(blank=False, null=False, max_length=32)
+    last_name = models.CharField(blank=False, null=False, max_length=32)
+    email = models.CharField(blank=True, null=True, max_length=128, validators=[validate_email], unique=True)
     phone = models.CharField(blank=True,null=True,max_length=11)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} <{self.email}>'
+
+    def __unicode__(self):
+        return f'{self.first_name} {self.last_name} <{self.email}>'
+
+
+class Checkin(models.Model):
+    contact_id = models.ForeignKey('Contact', on_delete=models.CASCADE)
+    employee_id = models.ForeignKey('Employee', on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
